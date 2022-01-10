59fc78a83d71e064f56e3236f47cfaa8e85cd623
chelsihoon <95158498+chelsihoon@users.noreply.github.com>
2022-01-10 11:47:50 +0900

    수정/변경ver.2.7

--- design_list.php
+++ design_list.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>49 WEEK 디자인 시안</title>
-    <link rel="stylesheet" href="css/draft.css">
+    <link rel="stylesheet" href="css/design_list.css">
 </head>
 <body>
     <a href="#" class="btn_top">TOP</a>
